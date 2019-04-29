ceb6f7eaf453ac422152a4ff416a4bc34a870e76
John clyde Dagpin <jcdagpin8@gmail.com>
2019-04-29 10:22:40 +0800

    anohter

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,14 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+# from django.views.generic import view
+from django.shortcuts import render, redirect
+# from django.contrib.auth import authenticate, login
+# from .forms import UserForm
+from django.contrib.auth import authenticate, login
 
-from django.shortcuts import render
+
+
 
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
@@ -10,3 +16,21 @@ class SignUp(generic.CreateView):
     template_name = 'signup.html'
 
 
+def register(request):
+    form = UserForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user.set_password(password)
+        user.save()
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                albums = Album.objects.filter(user=request.user)
+                return render(request, 'music/index.html', {'albums': albums})
+    context = {
+        "form": form,
+    }
+    return render(request, 'music/register.html', context)
